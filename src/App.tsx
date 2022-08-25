b017e08e59ab925a40185e9182711dc899b7179e
Alon <alon.horesh.08@gmail.com>
2022-08-25 13:21:50 +0300

    Add easter egg and a few more upgrades.

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,79 @@ function App() {
   const [prestige, setPrestige] = useState<number>(0)
   const [prestigeNeeded, setPrestigeNeeded] = useState<number>(10000000)
   const [canPrestige, setCanPrestige] = useState<boolean>(false)
+  const [availableUpgrades, setAvailableUpgrades] = useState<Upgrade[]>([
+    {
+      name: 'AntiCheat Bypass',
+      description: 'Allows you to click twice as fast!',
+      price: 25,
+      perm: true,
+    },
+    {
+      name: 'Grandma',
+      description: "A grandma's average cps is 2!",
+      price: 50,
+      perm: false,
+    },
+    {
+      name: 'Farm',
+      description: 'Farms MMC players for clicks. Average cps is 4!',
+      price: 200,
+      perm: false,
+    },
+    {
+      name: 'Jitter',
+      description:
+        'Jitter clicking can be quite good. But ur not good at it. Average cps is 8!',
+      price: 1000,
+      perm: false,
+    },
+    {
+      name: 'Jona',
+      description: 'Jona can click faster than you can. Average cps is 14!',
+      price: 10000,
+      perm: false,
+    },
+    {
+      name: 'Balls',
+      description:
+        "It's a new clicking method invented by Jona himself. It can do around 20 cps!",
+      price: 50000,
+      perm: false,
+    },
+    {
+      name: 'Autoclicker',
+      description: 'Autoclickers are the best. This one can click 28 cps!',
+      price: 100000,
+      perm: false,
+    },
+    {
+      name: 'OP Autoclicker',
+      description: 'OP Autoclickers are even better! Average cps is 56!',
+      price: 1000000,
+      perm: false,
+    },
+    {
+      name: 'God',
+      description:
+        'You prayed to God and he granted you the power of clicking! He can click 150 cps!',
+      price: 100000000,
+      perm: false,
+    },
+    {
+      name: 'Satan',
+      description:
+        "God didn't help enough, so you went to a darker place... He can click 666 cps!",
+      price: 1000000000,
+      perm: false,
+    },
+    {
+      name: 'Herobrine',
+      description:
+        'How did you find him? Anyways, he can click 100,000,000 cps!',
+      price: 10000000000,
+      perm: false,
+    },
+  ])
 
   useEffect(() => {
     try {
@@ -37,7 +110,14 @@ function App() {
     } catch (e) {}
   }, [])
 
+  useEffect(() => {
+    if (window.name === 'avi-go') {
+      setAvailableUpgrades((a) => [...a, aviEgozimUpgrade])
+    }
+  }, [window.name])
+
   function doPrestige() {
+    window.name = ''
     setCanPrestige(false)
     setPrestige((p) => p + 1)
     setCount(0)
@@ -118,61 +198,44 @@ function App() {
             2 *
             multiplier
       ) // 56 cps
+      setCount(
+        (c) =>
+          c + upgrades.filter((u) => u.name === 'God').length * 75 * multiplier
+      ) // 150 cps
+      setCount(
+        (c) =>
+          c +
+          upgrades.filter((u) => u.name === 'Satan').length *
+            75 *
+            2 *
+            2 *
+            1.11 *
+            multiplier
+      ) // 666 cps
+      setCount(
+        (c) =>
+          c +
+          upgrades.filter((u) => u.name === 'Herobrine').length *
+            50000000 *
+            multiplier
+      ) // 100,000,000 cps
+      setCount(
+        (c) =>
+          c +
+          upgrades.filter((u) => u.name === 'Avi Egozim').length *
+            5e16 *
+            multiplier
+      ) // 100,000,000,000,000 cps
     }, 500)
   }, [upgrades])
 
-  const availableUpgrades: Upgrade[] = [
-    {
-      name: 'AntiCheat Bypass',
-      description: 'Allows you to click twice as fast!',
-      price: 25,
-      perm: true,
-    },
-    {
-      name: 'Grandma',
-      description: "A grandma's average cps is 2!",
-      price: 50,
-      perm: false,
-    },
-    {
-      name: 'Farm',
-      description: 'Farms MMC players for clicks. Average cps is 4!',
-      price: 200,
-      perm: false,
-    },
-    {
-      name: 'Jitter',
-      description:
-        'Jitter clicking can be quite good. But ur not good at it. Average cps is 8!',
-      price: 1000,
-      perm: false,
-    },
-    {
-      name: 'Jona',
-      description: 'Jona can click faster than you can. Average cps is 14!',
-      price: 10000,
-      perm: false,
-    },
-    {
-      name: 'Balls',
-      description:
-        "It's a new clicking method invented by Jona himself. It can do around 20 cps!",
-      price: 50000,
-      perm: false,
-    },
-    {
-      name: 'Autoclicker',
-      description: 'Autoclickers are the best. This one can click 28 cps!',
-      price: 100000,
-      perm: false,
-    },
-    {
-      name: 'OP Autoclicker',
-      description: 'OP Autoclickers are even better! Average cps is 56!',
-      price: 1000000,
-      perm: false,
-    },
-  ]
+  const aviEgozimUpgrade: Upgrade = {
+    name: 'Avi Egozim',
+    description:
+      'Avi Egozim is the best. He can click 100,000,000,000,000,000 cps!', // 100 quadrillion cps
+    price: 0,
+    perm: true,
+  }
 
   return (
     <div>
