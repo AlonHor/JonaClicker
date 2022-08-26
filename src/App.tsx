614f8af8b41cda6977d09be6ba1eb08207ada5cc
Alon <alon.horesh.08@gmail.com>
2022-08-26 12:56:01 +0300

    Make available upgrades be in a different file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import jona from './assets/jona.png'
+import availableUpgradesImport from './availableUpgrades'
 
 function App() {
   interface Upgrade {
@@ -15,85 +16,9 @@ function App() {
   const [prestige, setPrestige] = useState<number>(0)
   const [prestigeNeeded, setPrestigeNeeded] = useState<number>(10000000)
   const [canPrestige, setCanPrestige] = useState<boolean>(false)
-  const [availableUpgrades, setAvailableUpgrades] = useState<Upgrade[]>([
-    {
-      name: 'Garfield',
-      description: 'Allows you to click as fast as Garfield! (1 cps... which is not really fast btw)',
-      price: 10,
-      perm: false,
-    },
-    {
-      name: 'AntiCheat Bypass',
-      description: 'Allows you to click twice as fast!',
-      price: 25,
-      perm: true,
-    },
-    {
-      name: 'Grandma',
-      description: "A grandma's average cps is 2!",
-      price: 50,
-      perm: false,
-    },
-    {
-      name: 'Farm',
-      description: 'Farms MMC players for clicks. Average cps is 4!',
-      price: 200,
-      perm: false,
-    },
-    {
-      name: 'Jitter',
-      description:
-        'Jitter clicking can be quite good. But ur not good at it. Average cps is 8!',
-      price: 1000,
-      perm: false,
-    },
-    {
-      name: 'Jona',
-      description: 'Jona can click faster than you can. Average cps is 14!',
-      price: 10000,
-      perm: false,
-    },
-    {
-      name: 'Balls',
-      description:
-        "It's a new clicking method invented by Jona himself. It can do around 20 cps!",
-      price: 50000,
-      perm: false,
-    },
-    {
-      name: 'Autoclicker',
-      description: 'Autoclickers are the best. This one can click 28 cps!',
-      price: 100000,
-      perm: false,
-    },
-    {
-      name: 'OP Autoclicker',
-      description: 'OP Autoclickers are even better! Average cps is 56!',
-      price: 1000000,
-      perm: false,
-    },
-    {
-      name: 'God',
-      description:
-        'You prayed to God and he granted you the power of clicking! He can click 150 cps!',
-      price: 100000000,
-      perm: false,
-    },
-    {
-      name: 'Satan',
-      description:
-        "God didn't help enough, so you went to a darker place... He can click 666 cps!",
-      price: 1000000000,
-      perm: false,
-    },
-    {
-      name: 'Herobrine',
-      description:
-        'How did you find him? Anyways, he can click 100,000,000 cps!',
-      price: 10000000000,
-      perm: false,
-    },
-  ])
+  const [availableUpgrades, setAvailableUpgrades] = useState<Upgrade[]>(
+    availableUpgradesImport
+  )
 
   useEffect(() => {
     try {
@@ -161,7 +86,9 @@ function App() {
       const multiplier = prestige === 0 ? 5 : prestigeMultiplier * prestige
       setCount(
         (c) =>
-          c + upgrades.filter((u) => u.name === 'Garfield').length / 2 * multiplier
+          c +
+          (upgrades.filter((u) => u.name === 'Garfield').length / 2) *
+            multiplier
       ) // 1 cps
       setCount(
         (c) =>
